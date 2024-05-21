025ccf9a9db6703a1ebbb8fa7928c038adcf907f
Haris90400 <haris90400@gmail.com>
2024-05-22 02:10:52 +0530

    Home Page frontend started

---
+++ lib/pages/chats_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatefulWidget {
+  const ChatsPage({super.key});
+
+  @override
+  State<ChatsPage> createState() => _ChatsPageState();
+}
+
+class _ChatsPageState extends State<ChatsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return _buildUi();
+  }
+
+  Widget _buildUi() {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:chatify/pages/chats_page.dart';
+import 'package:chatify/pages/users_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,12 +10,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentPage = 0;
+  final List<Widget> _pages = [
+    const ChatsPage(),
+    const UsersPage(),
+  ];
   @override
   Widget build(BuildContext context) {
     return _buildUi();
   }
 
   Widget _buildUi() {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[_currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPage,
+        onTap: (_index) {
+          setState(() {
+            _currentPage = _index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Chats',
+            icon: Icon(Icons.chat_bubble_sharp),
+          ),
+          BottomNavigationBarItem(
+            label: 'Users',
+            icon: Icon(
+              Icons.supervised_user_circle_sharp,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
